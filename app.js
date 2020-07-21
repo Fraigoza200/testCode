86fe0032254791e6003c9acdffec5d1605f395a2
Francisco Raigoza <raigoza.frankie@gmail.com>
2020-07-20 17:04:34 -0700

    code test

---
+++ app.js
@@ -0,0 +1,16 @@
+const fit = document.getElementById('image1')
+const c = document.getElementById('image2')
+const mat = document.getElementById('image3')
+let content = document.getElementById('description')
+
+
+
+function fitGuide(){
+    content.innerHTML === '' ? content.innerHTML='Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium at neque eveniet ipsum. Vero nulla perspiciatis esse maxime ipsam consectetur assumenda eum at voluptatem odio dolor alias, cumque, molestiae animi?': content.innerHTML=''
+}
+function care(){
+    content.innerHTML === '' ? content.innerHTML='Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium at neque eveniet ipsum. Vero nulla perspiciatis esse maxime ipsam consectetur assumenda eum at voluptatem odio dolor alias, cumque, molestiae animi?': content.innerHTML=''
+}
+function materials(){
+    content.innerHTML === '' ? content.innerHTML='<span class="content">Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium at neque eveniet ipsum. Vero nulla perspiciatis esse maxime ipsam consectetur assumenda eum at voluptatem odio dolor alias, cumque, molestiae animi?</span>': content.innerHTML=''
+}
